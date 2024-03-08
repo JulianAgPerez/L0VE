b0a78ce0217c1363a05f9f6771091f5d984deb1d
JulianAgPerez <julian.perez@alumnos.frm.utn.edu.ar>
2024-03-08 16:47:14 -0300

    Aparecen corazones al movimiento del mouse

--- heart_script.js
+++ heart_script.js
@@ -1,32 +1,19 @@
-function generateHeart(x, y) {
+document.addEventListener('mousemove',function(e){
     let body = document.querySelector('body');
     let heart = document.createElement('span');
-    heart.classList.add('heart');
-    heart.style.left = x + 'px';
-    heart.style.top = y + 'px';
+    let x = e.offsetX;
+    let y = e.offsetY;
+    heart.style.left = x+'px';
+    heart.style.top = y+'px';
 
     let size = Math.random() * 80;
-    heart.style.width = 20 + size + 'px';
-    heart.style.height = 20 + size + 'px';
+    heart.style.width = 20 + size+'px'
+    heart.style.height = 20 + size+'px'
     let transformValue = Math.random() * 360;
     heart.style.transform = 'rotate(' + transformValue + 'deg)';
 
     body.appendChild(heart);
-
-    setTimeout(function () {
+    setTimeout(function(){
         heart.remove();
-    }, 1000);
-}
-
-function onMouseMove(event) {
-    generateHeart(event.clientX, event.clientY);
-}
-
-function stopGeneratingHearts() {
-    document.removeEventListener('mousemove', onMouseMove);
-}
-
-document.addEventListener('mousedown', function () {
-    document.addEventListener('mousemove', onMouseMove);
-    document.addEventListener('mouseup',stopGeneratingHearts)
-});
+    },1000)
+})
